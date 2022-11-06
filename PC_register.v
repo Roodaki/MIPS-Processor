63f6ea31761851ec96d089cd92a2684988baadd5
AmirHossein Roodaki <amirhossein.rdk@gmail.com>
2022-11-06 16:28:46 +0330

    PC Register Updated: Hit Input Bit Added.

--- PC_register.v
+++ PC_register.v
@@ -1,7 +1,8 @@
 `timescale 1ns / 1ps
 
 // One of the 32 Registers of the MIPS Architecture Which Indicates the Executing Line of the Instruction Memory.
-module PC_register(input[31:0] next_instruction_address, input clock, output reg[31:0] instruction_address);
-	always @(posedge clock)
-		instruction_address = next_instruction_address;
+module PC_register(input[31:0] next_instruction_address, input hit, input clock, output reg[31:0] instruction_address);
+	always @(negedge clock)
+		if(hit)
+			#0.001 instruction_address = next_instruction_address;
 endmodule
--- Test Benches/PC_register_test.v
+++ Test Benches/PC_register_test.v
@@ -5,6 +5,7 @@ module PC_register_test;
 	// Inputs
 	reg [31:0] next_instruction_address;
 	reg clock;
+	reg hit;
 
 	// Outputs
 	wire [31:0] instruction_address;
@@ -13,23 +14,30 @@ module PC_register_test;
 	PC_register uut (
 		.next_instruction_address(next_instruction_address), 
 		.clock(clock), 
+		.hit(hit), 
 		.instruction_address(instruction_address)
 	);
 
 	initial
-	begin
-		next_instruction_address = 111;
-		clock = 0;
-		#100;
-		clock = 1;
-		#100;
-		clock = 0;
-		next_instruction_address = 222;
-		#100;
-		clock = 1;
-		#100;
-		clock = 0;
-		#100;
-	end
-	
+		begin
+			next_instruction_address = 111;
+			clock = 0;
+			hit = 0;
+			#100;
+			clock = 1;
+			#100;
+			clock = 0;
+			#100
+			clock = 1;
+			hit = 1;
+			#100
+			clock = 0;
+			#100;
+			next_instruction_address = 222;
+			clock = 1;
+			#100;
+			clock = 0;
+			#100;
+		end
+		
 endmodule
